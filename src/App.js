b345737cfafb171c8de8795eda170297c621b0be
Rohit Watharkar <Rohit-watharkar>
2022-12-19 20:02:53 +0530

    myapp2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-function App(){
-  return <h1>hello</h1>
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <h1></h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
