988b5528ae2dca1a0f095210d6a3d535a56cf680
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-19 23:47:08 +0530

    jsx first app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+// we call the function in the function with Attribute
 function App() {
   return (
     <div>
-      <h1>hello</h1>
-      <h1></h1>
+      <Person name="Rohit" />
+      <Person name="Ronit" />
+      <Person name="Rutu" />
+      <Person name="Sagar" />
+      <Person name="Vishal" />
+      <Person name="Kiran" />
+      <Person name="Vipul" />
+    </div>
+  )
+}
+
+// <Person />
+function Person() {
+  return (
+    <div>
+      <img src="https://picsum.photos/400" alt="" />
+      <h1>Rohit!!!! </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
+        delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
+        non. Quo cumque veniam nostrum voluptatem qui accusantium ipsa labore
+        libero! Tempore, consequuntur!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis
+        adipisci blanditiis sapiente doloremque consequatur quod nemo itaque,
+        deleniti accusamus fugit nihil architecto quidem voluptas repellendus
+        velit officia nulla vitae ullam.
+      </p>
     </div>
   )
 }
