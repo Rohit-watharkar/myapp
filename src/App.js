7b1dfe1914415de1a890723c800ef90056746827
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-20 00:25:56 +0530

    interpoloation function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-function App(){
-  return <h1>hello</h1>
+// USER DEFINED TAGS == COMPONENT
+function App() {
+  // Data Member
+  let project = 'my React project'
+  let username = 'Rohit'
+
+  // Member Function
+  return (
+    <div>
+      <h1>{project} </h1>
+      <h1>Hello {username}</h1>
+    </div>
+  )
 }
+
+export default App
