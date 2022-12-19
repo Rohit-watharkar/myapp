591cdcc2a762b6c40a4c5b1e801332e5c05927b9
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-20 01:19:11 +0530

    Data Menber With Menber function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-// USER DEFINED TAGS == COMPONENT
 function App() {
   // Data Member
-  let project = 'my React project'
-  let username = 'Rohit'
+  let project = 'Counter Application'
+  let btnvalu = 'click Mee'
 
   // Member Function
+  let clickMe = () => alert()
+  let increment = () => {}
+  let decrement = () => {}
+
+  // jsx
   return (
     <div>
-      <h1>{project} </h1>
-      <h1>Hello {username}</h1>
+      <h1>{project}</h1>
+      <input type="button" value={btnvalu} onClick={clickMe} />
+      <input type="button" value="Increment" onClick={increment} />
     </div>
   )
 }
