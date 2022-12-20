f7cc00cee0f7f89a25fb28c2a836105f2219edaa
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-20 20:27:28 +0530

    list paregraph dynamic csss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import { useState } from 'react'
 
 function App() {
+  // declare list for how many time Lorem declare
+  let [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1])
   // datamember
   let [theme, setTheme] = useState('primary')
 
@@ -27,33 +29,46 @@ function App() {
 
   return (
     <div>
-      {/* all theme assign to background  */}
-      <h1 className={`bg-${theme} text-light p-3`}>Bootstrap styling!!..</h1>
+      {/* this is uses for heading not move and other contain moveble */}
+      <div className="sticky-top">
+        {/* all theme assign to background  */}
+        <h1 className={`bg-${theme} text-light p-3`}>Bootstrap styling!!..</h1>
 
-      <input
-        //then given on the button
-        className="btn btn-primary"
-        type="button"
-        value="primary theme"
-        // call the function after clicking button
-        onClick={makeprimaryTheme}
-      />
-      <input
-        //then given on the button
-        className="btn btn-success"
-        type="button"
-        value="success theme"
-        // call the function after clicking button
-        onClick={makesuccessTheme}
-      />
-      <input
-        //then given on the button
-        className="btn btn-danger"
-        type="button"
-        value="danger theme"
-        // call the function after clicking button
-        onClick={makedangerTheme}
-      />
+        <input
+          //then given on the button
+          className="btn btn-primary"
+          type="button"
+          value="primary theme"
+          // call the function after clicking button
+          onClick={makeprimaryTheme}
+        />
+        <input
+          //then given on the button
+          className="btn btn-success"
+          type="button"
+          value="success theme"
+          // call the function after clicking button
+          onClick={makesuccessTheme}
+        />
+        <input
+          //then given on the button
+          className="btn btn-danger"
+          type="button"
+          value="danger theme"
+          // call the function after clicking button
+          onClick={makedangerTheme}
+        />
+      </div>
+
+      {/*one bye one paregram take from list */}
+      {list.map((item) => (
+        <div className={`alert alert-${theme} my-1`}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo
+          necessitatibus neque ratione rerum. Quaerat, error molestias. Est
+          quaerat veritatis temporibus consequuntur tempora quis tenetur
+          accusamus voluptatem repellat autem. Dolor, quia?
+        </div>
+      ))}
     </div>
   )
 }
