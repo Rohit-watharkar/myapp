13df08f5ecb65c1347737b3e97901a1a74b88dd0
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-21 00:04:49 +0530

    create basic mssage app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import { useState } from 'react'
 
 function App() {
-  let [message, setmessage] = useState('Rohit')
-
-  let updateMessage = (e) => {
-    // target returns the DOM element that triggered a specific event,
-    //  so we can retrieve any property/ attribute with a value
-    message = e.target.value
-
-    setmessage(message)
-  }
+  // declare list store message and time. new Date is the create object for  timening giving
+  let [list] = useState([
+    { message: 'hii Rohit', messageTime: new Date() },
+    { message: 'How Are You', messageTime: new Date() },
+    { message: 'I am Fine', messageTime: new Date() },
+    { message: 'And You', messageTime: new Date() },
+  ])
 
   return (
     <div>
-      <h1>Working with input</h1>
+      {/* give massage box heading */}
+      <h1 className="bg-primary text-white p-2">Messaging Demo</h1>
 
-      <input
-        type="text"
-        placeholder="Enter message"
-        value={message}
-        // The onChange event in React detects when the value of an input element changes.
-        onChange={updateMessage}
-      />
-      <h1>{message}</h1>
+      {/* taking one by one msg in the list */}
+      {list.map((item, index) => (
+        <div key={index} className="d-flex my-1">
+          <div className="badge text-bg-primary">
+            {/* print message */}
+            {item.message}
+
+            {/* give margine */}
+            <span className="ms-5">
+              {/* accessing object and print timing */}
+              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
+            </span>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
