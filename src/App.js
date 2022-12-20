5666dde39ab6d712a318e528d163fffd4bf0b766
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-20 17:56:39 +0530

    basic External Styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-// import file for external styling
-import './App.css'
-
+// import function writte in the index.css
+// bcz this is the uses in all function
+// import function writte in index.js file
 function App() {
-  let mystyle = { color: 'white', background: 'purple' }
-
   return (
     <div>
+      <h1>External Styling....</h1>
       <h1 className="beautify">Hello Rutu!!!</h1>
-
-      {/* THIS IS KIND OF INTERNAL.NOTE: only one curly braces. */}
-      <h1 style={mystyle}>Hello Rohit</h1>
-
-      {/* THIS IS INLINE STYLING */}
-      <h1 style={{ color: 'white', backgroundColor: 'gray' }}>
-        Hello Ronit!!!
-      </h1>
     </div>
   )
 }
