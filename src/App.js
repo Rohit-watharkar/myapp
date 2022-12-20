d87bd28c4baaee7c7076b5ded093017766786c99
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-21 01:06:12 +0530

    static messaging app

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import { useState } from 'react'
 
 function App() {
   // declare list store message and time. new Date is the create object for  timening giving
-  let [list] = useState([
+  let [list, setList] = useState([
     { message: 'hii Rohit', messageTime: new Date() },
     { message: 'How Are You', messageTime: new Date() },
     { message: 'I am Fine', messageTime: new Date() },
     { message: 'And You', messageTime: new Date() },
   ])
 
+  // member function
+  let addMessage = () => {
+    let newMessage = { message: 'hii everyone...!', messageTime: new Date() }
+    list = [newMessage, ...list]
+    setList(list)
+  }
+
   return (
     <div>
-      {/* give massage box heading */}
-      <h1 className="bg-primary text-white p-2">Messaging Demo</h1>
+      <h1 className="bg-primary text-white p-3">Messaging Done</h1>
 
-      {/* taking one by one msg in the list */}
+      <input
+        className="btn-btn-primary"
+        type="button"
+        value="Add Message"
+        onClick={addMessage}
+      />
+
       {list.map((item, index) => (
         <div key={index} className="d-flex my-1">
           <div className="badge text-bg-primary">
-            {/* print message */}
             {item.message}
-
-            {/* give margine */}
-            <span className="ms-5">
-              {/* accessing object and print timing */}
+            <span className="ms-4">
               {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
             </span>
           </div>
