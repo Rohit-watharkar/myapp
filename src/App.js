475052d5fb19d3c223e9942c55ca2d3e599849a2
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-20 17:18:35 +0530

    dynsmaic msg app

--- src/App.js
+++ src/App.js
@@ -6,18 +6,29 @@ function App() {
 
   //...list this is cloning use for take record as it is in the list
   let addItem = () => {
-    // logical part
-    let newList = [...list, 'Sangli islampur']
+    // after clicking button all reacord access using id
+    // reacord store in the textRef
+    // alternate :: NP DOM PLZ!
+    let textRef = document.querySelector('#textId1')
 
+    //  cloning list then all record store in newList
+    // logical part
+    let newList = [textRef.value, ...list]
+
+    // all record set in list
     // dom part
     setlist(newList)
   }
 
   return (
+    // first write in text area and after clicking button
     <div>
       <h1>{title}</h1>
+      <input type="text" name="" id="textId1" />
       <input type="button" value="Add Item into List" onClick={addItem} />
 
+      {/* all stored record print one bye one acces using map(item)
+       print on the browser */}
       {list.map((item) => (
         <h1>{item}</h1>
       ))}
