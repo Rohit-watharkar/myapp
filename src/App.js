14fb6c95994f80f89318600bafdd6c4699666f84
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-21 01:34:52 +0530

    msg add dynamically in the msg app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 function App() {
   // declare list store message and time. new Date is the create object for  timening giving
+  let [message, setMessage] = useState('Hello Rohit')
   let [list, setList] = useState([
     { message: 'hii Rohit', messageTime: new Date() },
     { message: 'How Are You', messageTime: new Date() },
@@ -10,22 +11,43 @@ function App() {
   ])
 
   // member function
+  let updateInputMessage = (e) => {
+    message = e.target.value
+    setMessage(message)
+  }
+
   let addMessage = () => {
-    let newMessage = { message: 'hii everyone...!', messageTime: new Date() }
+    // let newMessage = { message: 'hii everyone...!', messageTime: new Date() }
+    // dynamically msg givan at runtime
+    let newMessage = { message: message, messageTime: new Date() }
+    // ..list is cloning for give privious msg and also newMessage given
     list = [newMessage, ...list]
+
+    // this msg store in the list variable and this assign to setlist
     setList(list)
   }
 
   return (
     <div>
-      <h1 className="bg-primary text-white p-3">Messaging Done</h1>
+      {/* this is only heading of messaging app */}
+      <h1 className="bg-primary text-white p-3">Messaging App</h1>
 
-      <input
-        className="btn-btn-primary"
-        type="button"
-        value="Add Message"
-        onClick={addMessage}
-      />
+      <div className="d-flex">
+        <input
+          className="form-control"
+          value={message}
+          onChange={updateInputMessage}
+          type="text"
+          placeholder="Enter Message"
+        />
+
+        <input
+          className="btn-btn-primary"
+          type="button"
+          value="Add Message"
+          onClick={addMessage}
+        />
+      </div>
 
       {list.map((item, index) => (
         <div key={index} className="d-flex my-1">
