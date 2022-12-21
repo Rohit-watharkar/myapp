3c9558503f54525b2689e011bd1a19d069459cf4
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-21 23:57:18 +0530

    first navigation page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      {/** http://localhost:3000/ */}
-      <Route path="/" element={<h1>Home</h1>} />
+    <div>
+      <Link to="/home" className="fs-4">
+        Home |
+      </Link>
+      <Link to="/explore" className="fs-4">
+        Explore |
+      </Link>
+      <Link to="/notifications" className="fs-4">
+        Notifications |
+      </Link>
+      <Routes>
+        {/** http://localhost:3000/ */}
+        <Route path="/" element={<h1>Home</h1>} />
 
-      {/** http://localhost:3000/home */}
-      <Route path="/home" element={<h1>Home</h1>} />
+        {/** http://localhost:3000/home */}
+        <Route path="/home" element={<h1>Home</h1>} />
 
-      {/** http://localhost:3000/explore */}
-      <Route path="/explore" element={<h1>Explore</h1>} />
+        {/** http://localhost:3000/explore */}
+        <Route path="/explore" element={<h1>Explore</h1>} />
 
-      {/** http://localhost:3000/notifications */}
-      <Route path="/notifications" element={<h1>Notifications</h1>} />
+        {/** http://localhost:3000/notifications */}
+        <Route path="/notifications" element={<h1>Notifications</h1>} />
 
-      <Route path="*" element={<h1>Page Not Found</h1>} />
-    </Routes>
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </div>
   )
 }
 
