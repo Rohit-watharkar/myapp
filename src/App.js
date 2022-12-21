f93a1ea8e035995d19d96e7a70e44e62735ec596
Rohit Watharkar <watharkarrohit7975@gmail.com>
2022-12-21 15:52:09 +0530

    make Api call

--- src/App.js
+++ src/App.js
@@ -1,65 +1,29 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 function App() {
-  // declare list store message and time. new Date is the create object for  timening giving
-  let [message, setMessage] = useState('Hello Rohit')
-  let [list, setList] = useState([
-    { message: 'hii Rohit', messageTime: new Date() },
-    { message: 'How Are You', messageTime: new Date() },
-    { message: 'I am Fine', messageTime: new Date() },
-    { message: 'And You', messageTime: new Date() },
-  ])
+  let [title] = useState('API Demo')
+  let [messageList, setMessageList] = useState([])
 
   // member function
-  let updateInputMessage = (e) => {
-    message = e.target.value
-    setMessage(message)
-  }
+  let getAllMessages = async () => {
+    let url = `http://localhost:3001/messages`
+    let response = await axios.get(url)
 
-  let addMessage = () => {
-    // let newMessage = { message: 'hii everyone...!', messageTime: new Date() }
-    // dynamically msg givan at runtime
-    let newMessage = { message: message, messageTime: new Date() }
-    // ..list is cloning for give privious msg and also newMessage given
-    list = [newMessage, ...list]
-
-    // this msg store in the list variable and this assign to setlist
-    setList(list)
+    messageList = [...response.data]
+    setMessageList(messageList)
   }
 
   return (
     <div>
-      {/* this is only heading of messaging app */}
-      <h1 className="bg-primary text-white p-3">Messaging App</h1>
+      <h1>{title}</h1>
 
-      <div className="d-flex">
-        <input
-          className="form-control"
-          value={message}
-          onChange={updateInputMessage}
-          type="text"
-          placeholder="Enter Message"
-        />
-
-        <input
-          className="btn-btn-primary"
-          type="button"
-          value="Add Message"
-          onClick={addMessage}
-        />
-      </div>
-
-      {list.map((item, index) => (
-        <div key={index} className="d-flex my-1">
-          <div className="badge text-bg-primary">
-            {item.message}
-            <span className="ms-4">
-              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
-            </span>
-          </div>
-        </div>
+      <input type="button" value="Make/API call" onClick={getAllMessages} />
+      {messageList.map((item) => (
+        <div>{item.message}</div>
       ))}
     </div>
   )
 }
+
 export default App
